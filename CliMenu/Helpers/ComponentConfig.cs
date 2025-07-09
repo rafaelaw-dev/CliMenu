290ee649823a106b7034a3547664ce9f03dcce96
rafaelaw-dev <gcostadias14@gmail.com>
2025-07-09 11:33:55 +0100

    Updated ComponentConfig constructor

--- CliMenu/Helpers/ComponentConfig.cs
+++ CliMenu/Helpers/ComponentConfig.cs
@@ -1,3 +1,4 @@
+using CliMenu.Components.Base.Behaviors;
 using CliMenu.Interfaces;
 
 namespace CliMenu.Helpers;
@@ -9,16 +10,30 @@ namespace CliMenu.Helpers;
 /// </summary>
 /// <typeparam name="TDisp">Type of the display behavior, must implement <see cref="IDisplayer"/>.</typeparam>
 /// <typeparam name="TInput">Type of the input handler behavior, must implement <see cref="IInputHandler"/>.</typeparam>
-/// <param name="Disp">The display behavior implementation.</param>
-/// <param name="Input">The input handler implementation.</param>
-/// <param name="LifeCycle">The life cycle manager controlling component activation.</param>
-/// <param name="FocusManager">The focus manager controlling component focus state.</param>
-public sealed record ComponentConfig<TDisp, TInput>(TDisp Disp, TInput Input, ILifeCycle LifeCycle, IFocusManager FocusManager)
+public sealed record ComponentConfig<TDisp, TInput>
     where TDisp : IDisplayer
     where TInput : IInputHandler
 {
-    public TDisp Disp { get; } = Disp ?? throw new ArgumentNullException(nameof(Disp));
-    public TInput Input { get; } = Input ?? throw new ArgumentNullException(nameof(Input));
-    public ILifeCycle LifeCycle { get; } = LifeCycle ?? throw new ArgumentNullException(nameof(LifeCycle));
-    public IFocusManager FocusManager { get; } = FocusManager ?? throw new ArgumentNullException(nameof(FocusManager));
+    public TDisp Disp { get; }
+    public TInput Input { get; }
+    public IFocusManager FocusManager { get; }
+    public ILifeCycle LifeCycle { get; }
+    
+    /// <summary>
+    /// Immutable configuration record that encapsulates the default behaviors for a component.
+    /// Contains the display behavior, input handler, life cycle manager, and focus manager.
+    /// Ensures that none of the provided behaviors are null by validating in the constructor.
+    /// </summary>
+    /// <param name="disp">The display behavior implementation.</param>
+    /// <param name="input">The input handler implementation.</param>
+    /// <param name="lifeCycle">The life cycle manager controlling component activation.</param>
+    /// <param name="focusManager">The focus manager controlling component focus state.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public ComponentConfig(TDisp disp, TInput input, IFocusManager? focusManager = null, ILifeCycle? lifeCycle = null)
+    {
+        Disp = disp ?? throw new ArgumentNullException(nameof(disp));
+        Input = input ?? throw new ArgumentNullException(nameof(input));
+        FocusManager = focusManager ?? new FocusManager();
+        LifeCycle = lifeCycle ?? new LifeCycle();
+    }
 }
